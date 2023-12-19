a688f4d1c9cece9626baab4f56a09f17a10e5331
joel-2004 <jrjoel484@gmail.com>
2023-12-19 07:36:27 +0530

    FlatList

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,12 +1,14 @@
 
 import CurrentWeather from "./components/CurrentWeather";
 import UpcomingWeather from "./components/UpcomingWeather";
+import FlatListDemo from "./learn/2_FlatList";
 const App = () => {
 
   return (
     <>
       <CurrentWeather></CurrentWeather>
       <UpcomingWeather></UpcomingWeather>
+      <FlatListDemo></FlatListDemo>
     </>
   )
 }
---
+++ AwesomeProject/learn/2_FlatList.js
@@ -0,0 +1,38 @@
+import { View, Text, FlatList } from "react-native";
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+    },
+];
+const Item = ({ id, title }) => {
+    return (
+        <View>
+            <Text>{id}</Text>
+            <Text>{title}</Text>
+        </View>
+    );
+}
+//flat List supports just plain arrays like const data=["joel","me","etc"],as well as array of objects
+//section list only supports array of objects
+const FlatListDemo = () => {
+
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Flat List</Text>
+            <FlatList data={DATA} renderItem={({ item }) => <Item id={item.id} title={item.title}></Item>}
+                keyExtractor={item => item.id}
+            ></FlatList>
+            <Text>Hi</Text>
+        </View>)
+}
+
+export default FlatListDemo;
